8d6a8750a5aab0f2720a14cc3f916d6bcfa12070
Blackowl <192235135+blackowl1231@users.noreply.github.com>
2026-01-10 20:13:32 +0100

    Update Test.lua

--- Games/Test.lua
+++ Games/Test.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/2ad72035dd2dfda8d28b745d4cf87045919bc97d0acf5536f6656935841b8f9c/download"))()
+--loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/2ad72035dd2dfda8d28b745d4cf87045919bc97d0acf5536f6656935841b8f9c/download"))()
