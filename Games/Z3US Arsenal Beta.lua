ac85f28c171ee47dca6ae70cd084a7cd23e78f05
Blackowl <192235135+blackowl1231@users.noreply.github.com>
2026-01-11 16:29:00 +0100

    Update Z3US Arsenal Beta.lua

--- Games/Z3US Arsenal Beta.lua
+++ Games/Z3US Arsenal Beta.lua
@@ -1,1 +1,2 @@
+getgenv().SCRIPT_KEY = ""
 loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/edb83cf0f1c81ecc7357cccb96979d81cfb2cec3e0114a980dc46751f3ed86c7/download"))()
