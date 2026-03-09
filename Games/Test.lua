b27f3e716d1fc2db48ddf895da2c857deafd65d1
Blackowl <192235135+blackowl1231@users.noreply.github.com>
2026-03-09 20:07:16 +0100

    Update Test.lua

--- Games/Test.lua
+++ Games/Test.lua
@@ -1,2 +1,2 @@
-getgenv().SCRIPT_KEY = "KEYLESS"
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/2ad72035dd2dfda8d28b745d4cf87045919bc97d0acf5536f6656935841b8f9c/download"))()
+--getgenv().SCRIPT_KEY = "KEYLESS"
+--loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/2ad72035dd2dfda8d28b745d4cf87045919bc97d0acf5536f6656935841b8f9c/download"))()
